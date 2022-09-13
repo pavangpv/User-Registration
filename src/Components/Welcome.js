896e5c13c6ba4d51b8f7b78396e0e782a2e882a7
pavangpv <pavandhanu20@gmail.com>
2022-09-13 13:57:08 +0530

    complete

---
+++ src/Components/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Welcome =()=> {
+  return (
+    <div> 
+        <Header />
+        <h1>WELCOME my USer Happy Coding</h1>
+        </div>
+  )
+}
+
+export default Welcome
